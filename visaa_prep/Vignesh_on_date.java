207c68d167fd5bd85f8f9aec618407872e3ec42d
Soumya-Sangam <ssoumyassoumya4@gmail.com>
2024-11-13 21:59:43 +0530

    Create Vignesh_on_date.java

---
+++ visaa_prep/Vignesh_on_date.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        long x=sc.nextLong();
+        long y=sc.nextLong();
+        if(x<y){
+            System.out.println("NO");
+        }
+        else{
+            System.out.println("YES");
+        }
+    }
+}
