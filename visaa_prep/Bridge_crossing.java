0a0e9ebfeafda577b6785dd7993880723252432e
Soumya-Sangam <ssoumyassoumya4@gmail.com>
2024-11-13 22:37:53 +0530

    Create Bridge_crossing.java

---
+++ visaa_prep/Bridge_crossing.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        int y=sc.nextInt();
+        int z=sc.nextInt();
+        int a=z-y;
+        System.out.println(a/x);
+    }
+}
